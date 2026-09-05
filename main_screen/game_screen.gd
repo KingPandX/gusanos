3eb0e15b1144eb6447f47a84ce068825d9bf7008
Sergio Ricaflor <sergioricaflor@gmail.com>
2026-09-04 21:46:40 -0400

    fix: center combat zone on screen
    - Auto-center combat box on ready and window resize

--- main_screen/game_screen.gd
+++ main_screen/game_screen.gd
@@ -2,6 +2,13 @@ extends Control
 
 @onready var combat_box : PanelContainer = $CombatBox
 
+const FULL_SIZE = Vector2(720, 627)
+
 func _ready() -> void:
 	combat_box.pivot_offset = Vector2.ZERO
 	combat_box.scale = Vector2.ONE
+	_center_combat()
+	get_viewport().size_changed.connect(_center_combat)
+
+func _center_combat() -> void:
+	combat_box.position = (get_viewport_rect().size - FULL_SIZE) / 2
