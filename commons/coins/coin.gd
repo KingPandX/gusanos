3e805455a3129509f333f60c9add6030410a28d1
Sergio Ricaflor <sergioricaflor@gmail.com>
2026-09-05 16:43:39 -0400

    feat: animate coins with sprite sheet
    - Coin Sprite2D replaced with AnimatedSprite2D using Moneda-Sheet.png
    - Animated coin frames for visual feedback
    - Auto-collect logic moved from body_entered to _process loop

--- commons/coins/coin.gd
+++ commons/coins/coin.gd
@@ -4,9 +4,10 @@ class_name Coin
 @export var value: int = 1
 var collected: bool = false
 
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: AnimatedSprite2D = $Sprite2D
 
 func _ready() -> void:
+	sprite.play("default")
 	input_pickable = true
 	monitoring = false
 	monitorable = false
@@ -14,15 +15,15 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if collected:
 		return
+	if PlayerEffects.has_auto_collect():
+		if randf() < PlayerEffects.get_auto_collect_chance():
+			collect()
+			return
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var mouse_pos = get_global_mouse_position()
 		if global_position.distance_to(mouse_pos) < 20.0:
 			collect()
 
-func _on_body_entered(_body: Node) -> void:
-	if PlayerEffects.has_auto_collect():
-		if randf() < PlayerEffects.get_auto_collect_chance():
-			collect()
 
 func collect() -> void:
 	if collected:
